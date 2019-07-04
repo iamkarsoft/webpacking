71549c923a5826c30c893003d0e0810f9c630aa7
Kofi Ramos Amoussou <karsoft92@gmail.com>
2019-07-04 22:50:49 +0000

    init,added webpack-dev-server

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+module.exports ={
+  entry: './src/app.js',
+  output: {
+    filename:'js/app.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  
+  /**
+   * adding style loaders and css loaders
+   * npm install css-loader and style-loader
+   */
+    module: {
+      rules:[
+        {
+          test: /.\css&/,
+
+        }
+      ]
+    },
+  /** creating local server using webpack-dev-server
+  npm install webpack-dev-server --save 
+  **/
+  devServer:{
+    contentBase: path.join(__dirname,'dist'),
+    port: 6565,
+    compress: true
+  }
+}
